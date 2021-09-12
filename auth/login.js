3f2b75a7381fb1ebcfc92dbcd6d6e3424b38e876
aungkyawkyawsoe <akks.ygn@gmail.com>
2021-09-12 11:57:44 +0630

    added log

---
+++ auth/login.js
@@ -0,0 +1,12 @@
+const message = "Welcome from our website";
+const login = true;
+
+if(login) {
+    console.log("You are now logged in!");
+}
+
+// TODO: Create Express Server
+const express = "Express Server";
+console.log(express);
+
+console.log(message);
